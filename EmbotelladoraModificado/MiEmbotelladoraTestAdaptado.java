ee3f3c146bbfe98205a1a0a6e40e3b0f67663344
Adrián Payol Montero <35956442+apayol@users.noreply.github.com>
2018-12-05 21:59:44 +0100

    Adaptado nuestro test de embotelladora a su código

---
+++ EmbotelladoraModificado/MiEmbotelladoraTestAdaptado.java
@@ -0,0 +1,67 @@
+//javac -cp .:./hamcrest-core-1.3.jar:./junit-4.12.jar MiEmbotelladoraTestAdaptado.java
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import static org.junit.Assert.*;
+import java.util.*;
+
+//ADAPTAMOS NUESTRO CÓDIGO TEST A SU EMBOTELLADORA:
+
+public class MiEmbotelladoraTestAdaptado
+{
+	public int pequenas, grandes, total;
+
+	//Testing b1: grandes, pequenas o total son negativos
+	@Test (expected = RuntimeException.class)
+	public void negativeTest()
+	{
+		pequenas = -1;
+		grandes = 0;
+		total = 3;
+		Embotelladora.calculaBotellasPequenas(pequenas, grandes, total);
+	}
+	
+	//Testing b2: total > miCapacidad
+	@Test
+	public void b2Test()
+	{
+		pequenas = 4;
+		grandes = 1;
+		total = 20;
+		assertTrue ("b2Test", Embotelladora.calculaBotellasPequenas(pequenas, grandes, total) == -1);
+        //modificado para adaptarlo a su salida (-1)
+	}
+	
+	//Testing b3: total <= miCapacidad y total-5*grandes<0
+	@Test
+	public void b3Test()
+	{
+		pequenas = 2;
+		grandes = 5;
+		total = 5;
+		assertTrue ("b3Test", Embotelladora.calculaBotellasPequenas(pequenas, grandes, total) == 0);
+	}
+	
+	//Testing b4: total <= miCapacidad y total-5*grandes>0
+	@Test
+	public void b4Test()
+	{
+		pequenas = 3;
+		grandes = 1;
+		total = 7;
+		assertTrue ("b4Test", Embotelladora.calculaBotellasPequenas(pequenas, grandes, total) == 2);
+	}
+	
+	//Testing b5: total <= miCapacidad y total-5*grandes=0
+	@Test
+	public void b5Test()
+	{
+		pequenas = 0;
+		grandes = 4;
+		total = 20;
+		assertTrue ("b5Test", Embotelladora.calculaBotellasPequenas(pequenas, grandes, total) == 0);
+	}
+}
+		
+  
